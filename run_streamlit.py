e605ce9e8171c2c3016d4c1f9fde085283645157
KevinEB <86316923+KevinEB@users.noreply.github.com>
2022-01-28 21:12:55 +0700

    Update run_streamlit.py

--- run_streamlit.py
+++ run_streamlit.py
@@ -34,120 +34,120 @@ try:
     r = re.search(r"i\.(\d+)\.(\d+)", url)
     shop_id, item_id = r[1], r[2]      
     
-    threading.Thread(target=progress, args=()).start()
-    
-    ratings_url = "https://shopee.co.id/api/v2/item/get_ratings?filter=0&flag=1&itemid={item_id}&limit=20&offset={offset}&shopid={shop_id}&type=0"
-    offset = 0
-    d = {"username": [], "userid": [], "rating": [], "comment": [], "ctime": []}
-    while True:
-        data = requests.get(
-            ratings_url.format(shop_id=shop_id, item_id=item_id, offset=offset)).json()
+    with st.spinner('Wait for it...'):
+        ratings_url = "https://shopee.co.id/api/v2/item/get_ratings?filter=0&flag=1&itemid={item_id}&limit=20&offset={offset}&shopid={shop_id}&type=0"
+        offset = 0
+        d = {"username": [], "userid": [], "rating": [], "comment": [], "ctime": []}
+        while True:
+            data = requests.get(
+                ratings_url.format(shop_id=shop_id, item_id=item_id, offset=offset)).json()
 
-        # uncomment this to print all data:
-        # print(json.dumps(data, indent=4))
+            # uncomment this to print all data:
+            # print(json.dumps(data, indent=4))
 
-        i = 1
-        for i, rating in enumerate(data["data"]["ratings"], 1):
-            d["username"].append(rating["author_username"])
-            d["userid"].append(rating["userid"])
-            d["rating"].append(rating["rating_star"])
-            d["comment"].append(rating["comment"])
-            d["ctime"].append(rating["ctime"])
+            i = 1
+            for i, rating in enumerate(data["data"]["ratings"], 1):
+                d["username"].append(rating["author_username"])
+                d["userid"].append(rating["userid"])
+                d["rating"].append(rating["rating_star"])
+                d["comment"].append(rating["comment"])
+                d["ctime"].append(rating["ctime"])
 
-            print(rating["author_username"])
-            print(rating["userid"])
-            print(rating["rating_star"])
-            print(rating["comment"])
-            print(rating["ctime"])
-            print("-" * 100)
+                print(rating["author_username"])
+                print(rating["userid"])
+                print(rating["rating_star"])
+                print(rating["comment"])
+                print(rating["ctime"])
+                print("-" * 100)
 
-        if i % 20:
-            break
+            if i % 20:
+                break
 
-        offset += 20
+            offset += 20
 
 
-    df = pd.DataFrame(d)
-    print(df)
-    df.to_csv("data.csv", index=False)
+        df = pd.DataFrame(d)
+        print(df)
+        df.to_csv("data.csv", index=False)
 
-    df['comment'] = df.apply(lambda row: str(row['comment']).lower(), axis=1)
+        df['comment'] = df.apply(lambda row: str(row['comment']).lower(), axis=1)
 
-    tokenizer = RegexpTokenizer(r'\w+')
-    df['comment'] = df['comment'].apply(lambda x: ' '.join(word for word in tokenizer.tokenize(x)))
+        tokenizer = RegexpTokenizer(r'\w+')
+        df['comment'] = df['comment'].apply(lambda x: ' '.join(word for word in tokenizer.tokenize(x)))
 
-    df['review_length'] = df['comment'].apply(lambda x: len(x.split()))
+        df['review_length'] = df['comment'].apply(lambda x: len(x.split()))
 
-    df['date'] = pd.to_datetime(df['ctime'],unit='s').dt.date
-    df['time'] = pd.to_datetime(df['ctime'],unit='s').dt.time
+        df['date'] = pd.to_datetime(df['ctime'],unit='s').dt.date
+        df['time'] = pd.to_datetime(df['ctime'],unit='s').dt.time
 
-    mnr_df1 = df[['userid', 'date']].copy()
-    mnr_df2 = mnr_df1.groupby(by=['date', 'userid']).size().reset_index(name='mnr')
-    mnr_df2['mnr'] = mnr_df2['mnr'] / mnr_df2['mnr'].max()
-    df = df.merge(mnr_df2, on=['userid', 'date'], how='inner')
+        mnr_df1 = df[['userid', 'date']].copy()
+        mnr_df2 = mnr_df1.groupby(by=['date', 'userid']).size().reset_index(name='mnr')
+        mnr_df2['mnr'] = mnr_df2['mnr'] / mnr_df2['mnr'].max()
+        df = df.merge(mnr_df2, on=['userid', 'date'], how='inner')
 
-    review_data = df
-    res = OrderedDict()
+        review_data = df
+        res = OrderedDict()
 
-    for row in review_data.iterrows():
-        if row[1].userid in res:
-            res[row[1].userid].append(row[1].comment)
-        else:
-            res [row[1].userid] = [row[1].comment]
+        for row in review_data.iterrows():
+            if row[1].userid in res:
+                res[row[1].userid].append(row[1].comment)
+            else:
+                res [row[1].userid] = [row[1].comment]
 
-    individual_reviewer = [{'userid': k, 'comment': v} for k, v in res.items()]
-    df2 = dict()
-    df2['userid'] = pd.Series([])
-    df2['Maximum Content Similarity'] = pd.Series([])
-    vector = TfidfVectorizer(min_df=0)
-    count = -1
-    for reviewer_data in individual_reviewer:
-        count = count + 1
-        try:
-            tfidf = vector.fit_transform(reviewer_data['comment'])
-        except:
-            pass
-        cosine = 1 - pairwise_distances(tfidf, metric='cosine')
+        individual_reviewer = [{'userid': k, 'comment': v} for k, v in res.items()]
+        df2 = dict()
+        df2['userid'] = pd.Series([])
+        df2['Maximum Content Similarity'] = pd.Series([])
+        vector = TfidfVectorizer(min_df=0)
+        count = -1
+        for reviewer_data in individual_reviewer:
+            count = count + 1
+            try:
+                tfidf = vector.fit_transform(reviewer_data['comment'])
+            except:
+                pass
+            cosine = 1 - pairwise_distances(tfidf, metric='cosine')
 
-        np.fill_diagonal(cosine, -np.inf)
-        max = cosine.max()
+            np.fill_diagonal(cosine, -np.inf)
+            max = cosine.max()
 
-        # To handle reviewier with just 1 review
-        if max == -np.inf:
-            max = 0
-        df2['userid'][count] = reviewer_data['userid']
-        df2['Maximum Content Similarity'][count] = max
+            # To handle reviewier with just 1 review
+            if max == -np.inf:
+                max = 0
+            df2['userid'][count] = reviewer_data['userid']
+            df2['Maximum Content Similarity'][count] = max
 
-    df3 = pd.DataFrame(df2, columns=['userid', 'Maximum Content Similarity'])
+        df3 = pd.DataFrame(df2, columns=['userid', 'Maximum Content Similarity'])
 
-    # left outer join on original datamatrix and cosine dataframe
-    df = pd.merge(review_data, df3, on="userid", how="left")
-    df.drop(index=np.where(pd.isnull(df))[0], axis=0, inplace=True)
+        # left outer join on original datamatrix and cosine dataframe
+        df = pd.merge(review_data, df3, on="userid", how="left")
+        df.drop(index=np.where(pd.isnull(df))[0], axis=0, inplace=True)
 
-    with open ('logreg_pickle', 'rb') as rasengan:
-        logreg = pickle.load(rasengan)
+        with open ('logreg_pickle', 'rb') as rasengan:
+            logreg = pickle.load(rasengan)
 
-    X = df[['review_length', 'mnr', 'Maximum Content Similarity', 'rating']]
+        X = df[['review_length', 'mnr', 'Maximum Content Similarity', 'rating']]
 
-    y_pred = logreg.predict(X[['review_length', 'mnr', 'Maximum Content Similarity', 'rating']])
+        y_pred = logreg.predict(X[['review_length', 'mnr', 'Maximum Content Similarity', 'rating']])
 
-    X['fakeornot'] = y_pred
+        X['fakeornot'] = y_pred
 
-    fake = X.fakeornot.str.count("fake").sum()
-    original = X.fakeornot.str.count("original").sum()
+        fake = X.fakeornot.str.count("fake").sum()
+        original = X.fakeornot.str.count("original").sum()
 
-    print(fake)
-    print(original)
+        print(fake)
+        print(original)
 
-    labels = ['Komentar Tidak Membantu', 'Komentar Membantu',]
-    filter_opini = [fake, original]
-    colors = ['orange', 'seagreen']
-    explode = (0.1, 0)  # explode 1st slice
+        labels = ['Komentar Tidak Membantu', 'Komentar Membantu',]
+        filter_opini = [fake, original]
+        colors = ['orange', 'seagreen']
+        explode = (0.1, 0)  # explode 1st slice
 
-    fig1, ax1 = plt.subplots()
-    ax1.pie(filter_opini, explode=explode, labels=labels, colors=colors,
-            autopct='%1.1f%%', shadow=True, startangle=140)
-    ax1.axis('equal')
-    st.pyplot(fig1)
-except:
-    print("Owner Kevin")
+        fig1, ax1 = plt.subplots()
+        ax1.pie(filter_opini, explode=explode, labels=labels, colors=colors,
+                autopct='%1.1f%%', shadow=True, startangle=140)
+        ax1.axis('equal')
+        st.pyplot(fig1)
+   st.success("AKhirnya")
+ except:
+   print("Owner Kevin")
